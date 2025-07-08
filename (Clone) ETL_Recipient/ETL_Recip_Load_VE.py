6a162bd348764e6adad885580ba5576b3af8e4bf
Ahmad-Afzaal <Ahmad-Afzaal>
2025-07-08 17:19:29 +0000

    Received date changed to actual folder

--- (Clone) ETL_Recipient/ETL_Recip_Load_VE.py
+++ (Clone) ETL_Recipient/ETL_Recip_Load_VE.py
@@ -30,13 +30,17 @@
 
 # COMMAND ----------
 
+dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
 # DBTITLE 1,Parameters
-dbutils.widgets.text('received_date', '2024-03-20')
+dbutils.widgets.text('received_date', '/process/ETL_Rec')
 dbutils.widgets.text('catalog', 'oh_apm_stg')
 dbutils.widgets.text('schema_name', 'vendor_extracts')
 dbutils.widgets.text('ven_114fa', 'EDW_ven114fa_staging')
 dbutils.widgets.text('ven_115fa', 'EDW_ven115fa_staging')
-dbutils.widgets.text('s3_location', 's3://gia-stg-oh-ue1-data-raw/haven/inbound/VE_EDW/weekly/dt=')
+dbutils.widgets.text('s3_location', 's3://gia-stg-oh-ue1-data-raw/haven/inbound/VE_EDW')
 dbutils.widgets.text('ven_114fa_hist', 'EDW_ven114fa_historic')
 dbutils.widgets.text('ven_115fa_hist', 'EDW_ven115fa_historic')
 dbutils.widgets.text('Clng_Month_Gap', '-120')
--- (Clone) ETL_Recipient/ETL_Recip_Load_VE_Ctl.py
+++ (Clone) ETL_Recipient/ETL_Recip_Load_VE_Ctl.py
@@ -1,10 +1,14 @@
 # Databricks notebook source
+dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
 # DBTITLE 1,Parameters
 #-----------
 # DBX Parms
 #-----------
-dbutils.widgets.text('s3_location', 's3://gia-stg-oh-ue1-data-raw/haven/haven/inbound/VE_EDW/dt=')  
-dbutils.widgets.text('received_date', '2024-03-20')  
+dbutils.widgets.text('s3_location', 's3://gia-stg-oh-ue1-data-raw/haven/haven/inbound/VE_EDW')  
+dbutils.widgets.text('received_date', '/process/ETL_Rec')  
 dbutils.widgets.text('catalog', 'oh_apm_stg')  
 dbutils.widgets.text('schema_name', 'vendor_extracts')  
 dbutils.widgets.text('Clng_Month_Gap', '-120')
